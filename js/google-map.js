757be671e7ac8996e16362f1277a28dea9966091
David Q <davidmquick79@gmail.com>
2018-01-22 22:28:47 -0500

    Initial commit

---
+++ js/google-map.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 40.730610, lng: -73.935242};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
